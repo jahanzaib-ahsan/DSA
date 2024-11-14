5a75f68322159e80775ecd692e9122555b168d86
jahanzaib-ahsan <mail.jahanzaibahsan@gmail.com>
2024-11-14 12:59:55 -0600

    ceaser.cpp

---
+++ ceasercipher.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void encrypt(string t,int s)
+{
+    string e=t;
+
+}
+int main()
+{
+    int shift;
+    string encryp;
+    cout<<"Enter the text you want to encrypt ";
+    cin>>encryp;
+    cout<<"Enter the shift key : ";
+    cin>>shift;
+    encrypt(encryp,shift);
+    
+
+    
+}
